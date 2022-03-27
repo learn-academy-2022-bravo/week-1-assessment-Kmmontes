5199a5005419d3d325b04e9649c83e645ace3dc4
kmmontes <kmontes4989@gmail.com>
2022-03-27 14:03:12 -0700

    Updated answers to submit

--- code-challenges.js
+++ code-challenges.js
@@ -7,12 +7,12 @@
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in any two strings and returns the string with more characters. Use the two sets of test variables provided.
 
 // Set one:
-const fruit1 = "apple"
-const fruit2 = "banana"
+// const fruit1 = "apple"
+// const fruit2 = "banana"
 
 // Set two:
-const fruit3 = "cherry"
-const fruit4 = "kiwi"
+// const fruit3 = "cherry"
+// con  st fruit4 = "kiwi"
 
 
 // --------------------1) Create a function that takes in any given number and determines if the number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
@@ -21,29 +21,133 @@ const temp1 = 42
 const temp2 = 350
 const temp3 = 212
 
+// Pseudo Coding 
+// Create a function that takes any given number 
+// within the function determines if the number is below boiling, at boiling point or above boiling point
+// boiling point is 212 degrees Farenheit
+//I should expect 42 is below boiling point, 350 is above boiling point & 212 is at boiling point
+//within the function i would write and else if statement that determine is below boiling point is 42, else if above pointing is 350 and else if 212 is at boiling.
+// when I tried every number it gave the correct response
+// I received help with this question/challenge
+
+const boilingPoint = (temp) => {
+if(temp == 212){
+    return "${temp} is below boiling Point"
+} else if (temp>212){
+    return "${temp} is above the boiling point"
+}else if (temp< 212) {
+    return "${temp} is below the boiling point"
+}
+}
+
+console.log(boilingPoint(350))
+
+
+
+
+
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
+
+
+// Pseudo Coding
+// create a code that will combine two arrays using .concat()
+// ensure the length is returned by adding .length to another variable when console
+// logging.
+
 
 const myNumbers1 = [3, 7, 0, 36, -9]
 const myNumbers2 = [8, -7, 42, 9, 13]
 
+let twoArrays = myNumbers1.concat(myNumbers2)
+
+console.log(twoArrays.length)
+
+
+
+
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "1202 ovarB"
 
-const currentCohort = "Bravo 2022"
+// Psuedo Coding
+// Create a code that will reverse the letters of a string
+// I decleared I will reverse my variable of bravoCohort declared as "Bravo 2022"
+// In order to do that I will first split that will read each letter speratle.
+// Then I will make them reverse in an array by removing the ""
+// Then I will join them with the .join "" back to a string
+// It returned reversed as expected.
+
+
+const bravoCohort = "Bravo 2022"
+
+let reverse = bravoCohort.split("").reverse().join("")
+
+console.log(reverse)
 
 
 // --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
 
+// const myArray = [13, 34, 5, 10, 27, 42]
+
+// Psuedo Coding
+// Create a statement aka loop
+// The statment must evaluate which numbers in the array above classified as an even or odd value using the % 2
+// Use i++ so it will read all numbers continously in the array
+// The expected output should be in the order of "odd" "even" "odd" "even" "odd" "even"
+// After my first try"
+// I had to change my == to === to strictly equal my outcome
+// I had to make sure I read myArray.length to get a number
+// Following the past examples, help and trial I recived the correct outcome.
+
 const myArray = [13, 34, 5, 10, 27, 42]
+
+for (let i=0; i< myArray.length; i++){
+    if (myArray[i] %2 === 0){
+        console.log ("even") ;
+    }else if (myArray[i] %2 === 1){
+        console.log ("odd");
+    }
+}
+
+
 
 
 // --------------------5) Create a function that takes in a set of any two numbers and subtracts the smaller number from the larger number using the sets of test variables provided below. Expected output: 42, 3
 
 // Set one:
-const number1 = 58
-const number2 = 100
+// const number1 = 58
+// const number2 = 100
 
 // Set two:
-const number3 = 27
-const number4 = 24
+// const number3 = 27
+// const number4 = 24
+
+// Pseudo Coding
+// Create a function that takes in a set of any two numbers listed below.
+// In the function, you would subtract the smaller number from the larger number. 
+// Each function would need to be addressed with if and else if statements to cover all possibilities and ensure the largest number is subtracting the smallest number.
+// Expected outputs would be 42, 3
+
+// I received some help with this question
+
+// Functions:
+
+const set1 = (num1, num2) => { 
+    if(num1>num2) {
+        return num1-num2
+    }else if(num2>num1){
+        return num2-num1
+    }}
+
+
+console.log(set1 (27,24))
+
+const set2 = (num1, num2) => {
+    if(num1>num2) {
+        return num1-num2
+    }else if(num2>num1){
+        return num2-num1
+    }
+}
+
+console.log(set2 (100, 58))
--- interview-questions.md
+++ interview-questions.md
@@ -9,60 +9,97 @@ INSTRUCTOR EXAMPLE: What is a conditional statement?
 
   Your answer:
 
+<!-- //A condtional statment is a statment that returns a true or false response. A condtional statmemts also means that one is equal to another -->
+<!-- When I think of condtional of statments, I think of a statment that is clear in instruction and has a clear input that in turn produces a clear output. -->
+
   Researched answer:
 
-
+<!-- Condtional statements  have well defined instructions that produce a input dependent unique output. Condtional staments are like flow charts and help you create logic in your code. They include else/if statements and usually do return true/false statements also known as Boolean Values. -->
 
 1. What is git? What is the difference between git and Github?
 
   Your answer:
 
+<!-- Git is where each new version of the new code you create is tracked and Github is the software that stores the code you created. -->
+
+
   Researched answer:
 
+<!-- Git is a widley used open tool that is used to manage, share, and develop code. It is usually accessed through the terminal.-->
+<!-- Github is is the online network service that allows a developer to upload code generated from Git on to Github in order for the code to be reviewed, share and updated remotley by anyone. One needs the other for code to flow amongst developers.  -->
 
 
 2. Which JavaScript operators will return a Boolean value?
 
   Your answer:
 
+  <!-- /Boolean values return True or False -->
+  <!-- || OR -->
+
   Researched answer:
 
+<!-- The OR "||" operator runs a boolean value  -->
 
 
 3. What is an index? What is the difference between index and value?
 
   Your answer:
 
+  <!-- An index begins with and classified with a "0" and an index of "0" identifies the first value of an string.
+   <!-- A value is the data at a location.
+
+   Ex: ["Cookies", "Milk"]
+
+   index "0" would === "Cookies" 
+   value "Cookies" ==== "Cookies" -->
+
   Researched answer:
 
+<!-- An index is defined by the placement that each character holds in a string.Strings start a 0 index so the count of a string starts at 0. -->
 
+<!-- A value is the variable located with in the string that is representative of something like the xample given above. -->
 
 4. What is iteration?
 
   Your answer:
 
+<!-- <!-- When you run a code multiple times untill a condtion is met. Iterations are assoicated with loops.-->
+
   Researched answer:
 
-
+<!-- When you run a code multiples untill a condtion is met. Examples of an iteration would be a loop because it runs arguments in the process of one after the other and repeatedly. -->
+<!-- /You need to know how many times you want to repeat the code and what you want to repeat. --> 
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
   Your answer:
 
+<!-- One person is the driver who is the person writing the code and the other person is the navigator who is the person checking for errors and guiding the driver on the code to be inputted.  -->
+
   Researched answer:
 
+<!-- Pair programming is when two people write code on a machine. One person writes the code and the others tells the person essentially what code to write.  -->
 
+A value
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
 1. Higher Order Functions:
+<!-- functions that accepts functions as parameters and in turn gives you a function. I believe this is when we start talking about the sort and filter functions. -->
 
 2. Jest:
+<!-- A Javascript testing framwork used to test java code and ensure its correctness. I believe node command is part of jest? -->
 
 3. Objects:
 
+<!-- An object has three characteristics: 1. State, 2. Behavior and 3. Identiity. An objects has its own sets of variables. -->
+
 4. Method:
 
+<!-- A method is a block of code that only runs when its called and they can only run certain actions just like functions.  -->
+
 5. Classes:
+ -->
+<!-- Classess is like a framwork for creating objects. Classess are groups of variables of the same grouped in a class.  -->
